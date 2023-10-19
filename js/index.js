68b6c56b59a5b08f3aaccf6b0a876b1172beeebb
Vitor Domingos <vm486092@gmail.com>
2023-10-19 13:34:20 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Inicialização das variáveis 'valorPrimario' e 'valorSecundario'
+let valorPrimario = 40
+let valorSecundario = 15
+
+// Soma dos valores de 'valorPrimario' e 'valorSecundario'
+console.log(valorPrimario + valorSecundario)  // Deve imprimir 55
+
+// Subtração dos valores de 'valorPrimario' e 'valorSecundario'
+console.log(valorPrimario - valorSecundario)  // Deve imprimir 25
+
+// Divisão dos valores de 'valorPrimario' pelo valor de 'valorSecundario'
+console.log(valorPrimario / valorSecundario)  // Deve imprimir aproximadamente 2.6666666666666665
+
+// Multiplicação dos valores de 'valorPrimario' e 'valorSecundario'
+console.log(valorPrimario * valorSecundario)  // Deve imprimir 600
+
+// Cálculo do resto (módulo) da divisão de 'valorPrimario' por 'valorSecundario'
+console.log(valorPrimario % valorSecundario)  // Deve imprimir 10
